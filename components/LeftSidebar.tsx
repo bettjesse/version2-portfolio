c0a474bee4a0264e71371bd53506c78f3565508b
jesse <jessebett1999@gmail.com>
2023-10-12 14:34:20 +0300

    links

--- components/LeftSidebar.tsx
+++ components/LeftSidebar.tsx
@@ -3,17 +3,24 @@ import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa'; // Import Font
 
 const LeftSidebar = () => {
   return (
-    <section className="mt-[82px] sticky left-0  top-0 z-20 flex h-screen w-fit flex-col justify-center mx-auto gap-24 overflow-auto bg-[#0A192F] pb-5 px-8 max-md:hidden custom-scrollbar">
-      <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
-        <FaGithub size={24} className= "text-white hover:text-cyan-100" /> {/* GitHub icon */}
-      </div>
-      <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
-        <FaLinkedin size={24}  className= "text-white hover:text-cyan-100" /> {/* LinkedIn icon */}
-      </div>
-      <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
-        <FaTwitter size={24}  className= "text-white hover:text-cyan-100" /> {/* Twitter icon */}
-      </div>
-    </section>
+    <section className="mt-[82px] sticky left-0 top-0 z-20 flex h-screen w-fit flex-col justify-center mx-auto gap-24 overflow-auto bg-[#0A192F] pb-5 px-8 max-md:hidden custom-scrollbar">
+    <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
+      <a href="https://github.com/bettjesse" target="_blank" rel="noopener noreferrer">
+        <FaGithub size={24} className="text-white hover:text-cyan-100" /> {/* GitHub icon */}
+      </a>
+    </div>
+    <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
+      <a href="https://www.linkedin.com/in/jesse-bett-46b0aa1a8/" target="_blank" rel="noopener noreferrer">
+        <FaLinkedin size={24} className="text-white hover:text-cyan-100" /> {/* LinkedIn icon */}
+      </a>
+    </div>
+    <div className="cursor-pointer text-center transform transition-transform duration-300 hover:translate-y-[-5px] hover:scale-110">
+      <a href="https://twitter.com/Jesse_bett" target="_blank" rel="noopener noreferrer">
+        <FaTwitter size={24} className="text-white hover:text-cyan-100" /> {/* Twitter icon */}
+      </a>
+    </div>
+  </section>
+  
   );
 };
 
