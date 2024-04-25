91e73397d154c1feaa055fe23b7bddafe4a7b641
Jesee Bett <98686980+bettjesse@users.noreply.github.com>
2024-04-25 12:53:29 +0300

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar: React.FC = () => {
               <Link href="#contact">Contact</Link>
             </div>
             <a
-              href="https://drive.google.com/file/d/1rrVofLyOW7o9EA_qiEoQwP38pAMavwNw/view?usp=sharing"
+              href="https://drive.google.com/file/d/1-muVZVmnQeO_yLm1EpWNWifVytlhcFow/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
             >
